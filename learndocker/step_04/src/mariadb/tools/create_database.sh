5bf1246d5a4a2077227061b31c16c153d0b5be04
Tanin Nantakitgosol <tnantaki@z3t4c1.42bangkok.com>
2023-12-26 07:50:20 +0700

    start step04

---
+++ learndocker/step_04/src/mariadb/tools/create_database.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+
+while true;
+service mariadb start
+
+if [ -d "/var/lib/mysql/$DB_NAME" ]
+then
+	echo "Database already created"
+else
+	echo "Create new database..."
+	#create new database
+	mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+	#create new user and password
+	mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+	#set permission for database with every table to user
+	mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+	#reload grant table immediately
+	mariadb -e "FLUSH PRIVILEGES;"
+	echo "Create database success"
+fi
+
+mariadb
