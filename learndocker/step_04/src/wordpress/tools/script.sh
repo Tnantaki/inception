b06c025bea1095e5fb008a890d80601902af57a1
Tanin Nantakitgosol <tnantaki@z2t9c2.42bangkok.com>
2023-12-29 11:14:06 +0700

    update 04
    add args

--- learndocker/step_04/src/wordpress/tools/script.sh
+++ learndocker/step_04/src/wordpress/tools/script.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-mkdir -p /run/php;
-if [ ! -f "var/www/html/wp-config.php" ]
+if [ -e "/var/www/html/wordpress/wp-config-sample.php" ]
 then
-mkdir -p /var/www/html
-cd /var/www/html
-wp core download --allow-root
-
-cp wp-config-sample.php wp-config.php
-sed -i "s/username_here/$DB_USER/g" wp-config.php
-sed -i "s/password_here/$DB_PASSWD/g" wp-config.php
-sed -i "s/localhost/$DB_HOST/g" wp-config.php
-sed -i "s/database_name_here/$DB_NAME/g" wp-config.php
-# wp core install --url=$DOMAIN_NAME --title=$TITLE \
-#     --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWD \
-#     --admin_email=$WP_ADMIN_EMAIL --allow-root
-# wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWD --allow-root
-
+	cd /var/www/html/wordpress
+	mv wp-config-sample.php wp-config.php
+	sed -i "s/database_name_here/${DB_NAME}/g" wp-config.php
+	sed -i "s/username_here/${DB_USER}/g" wp-config.php
+	sed -i "s/password_here/${DB_PASSWORD}/g" wp-config.php
 fi
-
-exec "$@"
