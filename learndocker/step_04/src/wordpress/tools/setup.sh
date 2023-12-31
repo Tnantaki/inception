d8934e27ba4da2cb283f7595c256e558754f9224
Tanin Nantakitgosol <tnantaki@z2t9c2.42bangkok.com>
2024-01-01 00:56:41 +0700

    update

--- learndocker/step_04/src/wordpress/tools/setup.sh
+++ learndocker/step_04/src/wordpress/tools/setup.sh
@@ -9,11 +9,4 @@ then
 	sed -i "s/username_here/${DB_USER}/g" wp-config.php
 	sed -i "s/password_here/${DB_PASSWORD}/g" wp-config.php
 	sed -i "s/localhost/${DB_HOSTNAME}/g" wp-config.php
-	# create admin and user
-	# wp core install --url=${DOMAIN_NAME} \
-	# 	--title=${WP_ADTITLE} \
-	# 	--admin_user=${WP_ADUSER} \
-	# 	--admin_password=${WP_ADPASS} \
-	# 	--admin_email=${WP_ADEMAIL} --allow-root
-	# wp config create ${WP_USER} ${WP_EMAIL} --user_pass=${WP_PASS} --allow-root
 fi
